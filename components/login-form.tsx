3e2cebd010030dd5d0a3717d681e7a85017a6c3e
Mohd Rushdan Ashraf Mohd Isha <rushdan5445@gmail.com>
2026-02-18 11:38:38 +0800

    Update build script in package.json to include Prisma generation and modify login form layout for improved accessibility and styling.

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -78,13 +78,11 @@ export function LoginForm({ className, ...props }: React.ComponentProps<"form">)
         </Field>
 
         <Field>
-          <div className="flex items-center">
-            <FieldLabel htmlFor="password">Password</FieldLabel>
-            <a href="#" className="ml-auto text-sm underline-offset-4 hover:underline">
-              Forgot your password?
-            </a>
-          </div>
+          <FieldLabel htmlFor="password">Password</FieldLabel>
           <Input id="password" name="password" type="password" required />
+          <a href="#" className="text-xs underline-offset-4 hover:underline">
+            Forgot your password?
+          </a>
         </Field>
 
         <Field>
